78daff45247f4357403c56a4980125cd9ff8ab80
Bas van Opheusden <basvanopheusden@gmail.com>
2025-06-27 23:37:16 -0700

    Remove rule references from non-Magic tests

--- tests/core/test_playerstate_validation.py
+++ tests/core/test_playerstate_validation.py
@@ -1,15 +1,14 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
 import pytest
 
-from magic_combat import PlayerState, DEFAULT_STARTING_LIFE
+from magic_combat import DEFAULT_STARTING_LIFE, PlayerState
 
 
 def test_negative_life_init():
-    """CR 107.1: Numbers like life totals can't be negative."""
     with pytest.raises(ValueError):
         PlayerState(life=-1, creatures=[])
 
 
 def test_negative_poison_init():
-    """CR 107.1: Numbers like poison counters can't be negative."""
     with pytest.raises(ValueError):
         PlayerState(life=DEFAULT_STARTING_LIFE, creatures=[], poison=-1)
